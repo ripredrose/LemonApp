0461541025deb5a38ec1ba876da6dadfd02695c5
ripredrose <79838994+ripredrose@users.noreply.github.com>
2024-02-07 17:53:59 +0100

    sg

---
+++ src/components/Testimonials.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Testimonials = () => {
+  const reviews_info = [
+    {
+      name: "Amir Seilsepour",
+      stars: 5,
+      opinion: "Love it"
+    },
+    {
+      name: "Ashwin Vaswani",
+      stars: 4,
+      opinion: "Nice but found rat"
+    },
+    {
+      name: "Imansyah Muhamad Putera",
+      stars: 4,
+      opinion: "Nice spaghetti"
+    },
+    {
+      name: "Judith Ejike",
+      stars: 5,
+      opinion: "I like it"
+    }
+  ]
+  
+  return (
+    <section className='testimonials'>
+        <header className='section-title'>What our customers say!</header>
+        <section className='opinions '>
+            {reviews_info.map((review)=>{
+              return  <article className='opinion'>
+                        <div className='rating'>
+                          {Array.from(Array(review.stars)).map((_) => <img src="/assets/star" alt="star" className='star'/>)}
+                        </div>
+                        <div className='customer-info'>
+                            <img src={"/assets/"+review.name} alt='customer_image' className='profile'></img>
+                            <h5>{review.name}</h5>
+                        </div>
+                        <div className='highlight-text review-text'>{review.opinion}</div>
+                      </article>
+            })
+
+            }
+        </section>
+    </section>
+  )
+}
+
+export default Testimonials
